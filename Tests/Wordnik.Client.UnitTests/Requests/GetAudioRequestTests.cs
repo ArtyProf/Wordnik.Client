2ebe45fd7472cf8bb9f2b4f436fb1de2612a3ef6
Artur Khutak <arty4prof@gmail.com>
2025-09-10 16:41:39 +0300

    add unit tests

---
+++ Tests/Wordnik.Client.UnitTests/Requests/GetAudioRequestTests.cs
@@ -0,0 +1,44 @@
+﻿using Wordnik.Client.Requests;
+
+namespace Wordnik.Client.UnitTests.Requests;
+
+public class GetAudioRequestTests
+{
+    [Theory]
+    [InlineData(
+           "apple", false, 0,
+           "word=apple&useCanonical=false"
+       )]
+    [InlineData(
+           "ball", true, 0,
+           "word=ball&useCanonical=true"
+       )]
+    [InlineData(
+           "car", null, 1,
+           "word=car&limit=1"
+       )]
+    [InlineData(
+           "car", false, 1,
+           "word=car&useCanonical=false&limit=1"
+       )]
+    public void ToString_ShouldGenerateCorrectQueryString(
+       string word,
+       bool? useCanonical,
+       int limit,
+       string expectedQueryString)
+    {
+        // Arrange
+        var request = new GetAudioRequest
+        {
+            Word = word,
+            UseCanonical = useCanonical,
+            Limit = limit
+        };
+
+        // Act
+        var queryString = request.ToString();
+
+        // Assert
+        Assert.Equal(expectedQueryString, queryString);
+    }
+}
