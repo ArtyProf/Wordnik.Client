4cc7cf93c2b4432e6f76559e9666a6b100e0b986
Artur Khutak <arty4prof@gmail.com>
2025-09-11 15:29:16 +0300

    add integration tests

---
+++ Tests/Wordnik.Client.IntegrationTests/WordnikClientIntegrationTests/PronunciationIntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿using Wordnik.Client.Enums;
+using Wordnik.Client.Helpers;
+using Wordnik.Client.Requests;
+
+namespace Wordnik.Client.IntegrationTests.WordnikClientIntegrationTests;
+
+public class PronunciationIntegrationTests : IntegrationTestBase
+{
+    [Theory]
+    [InlineData("example", 3)]
+    public async Task GetPronunciations_WhenCalled_ShouldReturnDefinitions(string word, int limit)
+    {
+        // Arrange
+        var client = new WordnikClient(_httpClient, _apiKey);
+
+        var request = new GetPronunciationRequest
+        {
+            Word = word,
+            Limit = limit
+        };
+
+        // Act
+        await ThrottleAsync();
+        var pronunciations = await client.GetPronunciationAsync(request);
+
+        // Assert
+        Assert.NotNull(pronunciations);
+        Assert.NotEmpty(pronunciations);
+
+        foreach (var pronunciation in pronunciations)
+        {
+            if (pronunciation.RawType == FormatType.Ahd5.ToApiString())
+            {
+                Assert.True(pronunciation.Id.Length > 0);
+            }
+            Assert.True(pronunciation.Raw.Length > 0);
+            Assert.True(pronunciation.RawType.Length > 0);
+            Assert.True(pronunciation.AttributionText.Length > 0);
+            Assert.True(pronunciation.AttributionUrl.Length > 0);
+        }
+    }
+}
