49532fc11cd6b984832f6a604f48c17e168b389c
Artur Khutak <arty4prof@gmail.com>
2025-09-12 14:34:49 +0300

    Update Assembly version to v.1.0.0 (#32)
    * update Assembly version to v.1.0.0
    * make LangVersion be compatible with .net standart 2.1
    * fix issues after setting up LangVersion

--- Wordnik.Client/Responses/AudioResponse.cs
+++ Wordnik.Client/Responses/AudioResponse.cs
@@ -1,79 +1,80 @@
 ﻿using System;
 
-namespace Wordnik.Client.Responses;
-
-/// <summary>
-/// Represents metadata for a specific audio file associated with a word.
-/// </summary>
-public class AudioResponse
+namespace Wordnik.Client.Responses
 {
     /// <summary>
-    /// Gets or sets the unique identifier for this audio metadata.
+    /// Represents metadata for a specific audio file associated with a word.
     /// </summary>
-    public int Id { get; set; }
+    public class AudioResponse
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for this audio metadata.
+        /// </summary>
+        public int Id { get; set; }
 
-    /// <summary>
-    /// Gets or sets the word for which this audio file is relevant.
-    /// </summary>
-    public string Word { get; set; }
+        /// <summary>
+        /// Gets or sets the word for which this audio file is relevant.
+        /// </summary>
+        public string Word { get; set; }
 
-    /// <summary>
-    /// Gets or sets the type of the audio, such as 'pronunciation'.
-    /// </summary>
-    public string AudioType { get; set; }
+        /// <summary>
+        /// Gets or sets the type of the audio, such as 'pronunciation'.
+        /// </summary>
+        public string AudioType { get; set; }
 
-    /// <summary>
-    /// Gets or sets the public URL to access the audio file.
-    /// </summary>
-    public string FileUrl { get; set; }
+        /// <summary>
+        /// Gets or sets the public URL to access the audio file.
+        /// </summary>
+        public string FileUrl { get; set; }
 
-    /// <summary>
-    /// Gets or sets the duration of the audio file in seconds.
-    /// </summary>
-    public double Duration { get; set; }
+        /// <summary>
+        /// Gets or sets the duration of the audio file in seconds.
+        /// </summary>
+        public double Duration { get; set; }
 
-    /// <summary>
-    /// Gets or sets the text attribution for the audio file.
-    /// </summary>
-    public string AttributionText { get; set; }
+        /// <summary>
+        /// Gets or sets the text attribution for the audio file.
+        /// </summary>
+        public string AttributionText { get; set; }
 
-    /// <summary>
-    /// Gets or sets the URL for the source of the attribution.
-    /// </summary>
-    public string AttributionUrl { get; set; }
+        /// <summary>
+        /// Gets or sets the URL for the source of the attribution.
+        /// </summary>
+        public string AttributionUrl { get; set; }
 
-    /// <summary>
-    /// Gets or sets the date and time when the audio was created.
-    /// </summary>
-    public DateTime CreatedAt { get; set; }
+        /// <summary>
+        /// Gets or sets the date and time when the audio was created.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
 
-    /// <summary>
-    /// Gets or sets the identifier for the user or entity that created the audio file.
-    /// </summary>
-    public string CreatedBy { get; set; }
+        /// <summary>
+        /// Gets or sets the identifier for the user or entity that created the audio file.
+        /// </summary>
+        public string CreatedBy { get; set; }
 
-    /// <summary>
-    /// Gets or sets the description of the audio file.
-    /// </summary>
-    public string Description { get; set; }
+        /// <summary>
+        /// Gets or sets the description of the audio file.
+        /// </summary>
+        public string Description { get; set; }
 
-    /// <summary>
-    /// Gets or sets the number of votes received for the audio file.
-    /// </summary>
-    public int VoteCount { get; set; }
+        /// <summary>
+        /// Gets or sets the number of votes received for the audio file.
+        /// </summary>
+        public int VoteCount { get; set; }
 
-    /// <summary>
-    /// Gets or sets the average vote rating for the audio file.
-    /// </summary>
-    public double VoteAverage { get; set; }
+        /// <summary>
+        /// Gets or sets the average vote rating for the audio file.
+        /// </summary>
+        public double VoteAverage { get; set; }
 
-    /// <summary>
-    /// Gets or sets the weighted average vote rating for the audio file.
-    /// </summary>
-    public double VoteWeightedAverage { get; set; }
+        /// <summary>
+        /// Gets or sets the weighted average vote rating for the audio file.
+        /// </summary>
+        public double VoteWeightedAverage { get; set; }
 
-    /// <summary>
-    /// Gets or sets the number of comments associated with the audio file.
-    /// </summary>
-    public int CommentCount { get; set; }
+        /// <summary>
+        /// Gets or sets the number of comments associated with the audio file.
+        /// </summary>
+        public int CommentCount { get; set; }
+    }
 }
