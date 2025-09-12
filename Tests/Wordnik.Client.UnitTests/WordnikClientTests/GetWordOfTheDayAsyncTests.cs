1265d790e420baac8e8e9718c688649cb1eafaeb
Artur Khutak <arty4prof@gmail.com>
2025-09-12 14:14:18 +0300

    improve coverage

--- Tests/Wordnik.Client.UnitTests/WordnikClientTests/GetWordOfTheDayAsyncTests.cs
+++ Tests/Wordnik.Client.UnitTests/WordnikClientTests/GetWordOfTheDayAsyncTests.cs
@@ -13,7 +13,35 @@ public class GetWordOfTheDayAsyncTests
     {
         var responseContent = JsonConvert.SerializeObject(new WordOfTheDayResponse
         {
-            Word = "A representative form or pattern."
+            Id = "68899e2b924d55d89fb4c8f2",
+            Word = "example",
+            ContentProvider = new ContentProvider
+            {
+                Id = 711,
+                Name = "Wordnik"
+            },
+            Definitions =
+            [
+                new() {
+                    Source = "gcide",
+                    Text = "A representative form or pattern.",
+                    Note = "No additional notes.",
+                    PartOfSpeech = "noun"
+                }
+            ],
+                PublishDate = new DateTime(2025, 09, 01),
+                Examples =
+            [
+                new() {
+                    Url = "http://example.com/example",
+                    Title = "Example Usage",
+                    Text = "This is an example sentence for the word 'example'.",
+                    Id = 123456789
+                }
+            ],
+            PlainProductionDate = "2025-09-01",
+            HtmlExtra = "<div>This is extra HTML content.</div>",
+            Note = "Derived from Latin word exemplum."
         });
 
         var request = new GetWordOfTheDayRequest();
@@ -27,7 +55,32 @@ public class GetWordOfTheDayAsyncTests
             assertions: response =>
             {
                 Assert.NotNull(response);
-                Assert.Equal("A representative form or pattern.", response.Word);
+                Assert.Equal("68899e2b924d55d89fb4c8f2", response.Id);
+                Assert.Equal("example", response.Word);
+                Assert.Equal(new DateTime(2025, 09, 01), response.PublishDate);
+                Assert.Equal("2025-09-01", response.PlainProductionDate);
+                Assert.Equal("<div>This is extra HTML content.</div>", response.HtmlExtra);
+                Assert.Equal("Derived from Latin word exemplum.", response.Note);
+
+                Assert.NotNull(response.ContentProvider);
+                Assert.Equal(711, response.ContentProvider.Id);
+                Assert.Equal("Wordnik", response.ContentProvider.Name);
+
+                Assert.NotNull(response.Definitions);
+                Assert.NotEmpty(response.Definitions);
+                var definition = response.Definitions.First();
+                Assert.Equal("gcide", definition.Source);
+                Assert.Equal("A representative form or pattern.", definition.Text);
+                Assert.Equal("No additional notes.", definition.Note);
+                Assert.Equal("noun", definition.PartOfSpeech);
+
+                Assert.NotNull(response.Examples);
+                Assert.NotEmpty(response.Examples);
+                var example = response.Examples.First();
+                Assert.Equal("http://example.com/example", example.Url);
+                Assert.Equal("Example Usage", example.Title);
+                Assert.Equal("This is an example sentence for the word 'example'.", example.Text);
+                Assert.Equal(123456789, example.Id);
             });
     }
 
